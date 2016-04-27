421654f7c4e143ca84bb42587ced0c7e8440d7be
Chris Schule <cmschule16@gmail.com>
2016-04-26 19:24:30 -0500

    Added files via upload
    Cut down primeNumber from running 7.3s to 0.9s by changing the inner loop to only divide by previously found prime numbers

---
+++ primeNumber.js
@@ -0,0 +1,20 @@
+function primeNumber(primePosition) {
+	var count = 6;
+	var num = [2, 3, 5, 7, 11, 13];
+	var currentNum = 13;
+	while (count < primePosition) {
+		currentNum += 2;
+		for (var i = 0; i < num.length; i++) {
+			if (currentNum%num[i] === 0) {
+				break;
+			}
+			else if (i === num.length-1) {
+				num.push(currentNum)
+				count++;
+			}
+		}
+	}
+	return num;
+}
+
+console.log(primeNumber(10001));
