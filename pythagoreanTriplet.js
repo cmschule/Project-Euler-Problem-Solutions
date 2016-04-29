5dd09b4db5aa42a9a3f4b00c0c333c6d79d7be17
Chris Schule <cmschule16@gmail.com>
2016-04-28 21:56:55 -0500

    Added files via upload

--- pythagoreanTriplet.js
+++ pythagoreanTriplet.js
@@ -1,7 +1,7 @@
 function pythagoreanTriplet(sum) {
 	var b;
 	var c;
-	for (var a = 1; sum/3; a++) {
+	for (var a = 1; a < sum/3; a++) {
 		//found by equating a^2+b^2 to (sum-a-b)^2
 		b = (-sum*sum+2*sum*a)/(-2*sum+2*a);
 		//to check if b is a natural number
